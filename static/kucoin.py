5ee5ed41a3509dfa45cbd456e52afce0386640d7
Fábio <fberbert@gmail.com>
2019-08-08 04:12:52 -0300

    kucoin

--- static/kucoin.py
+++ static/kucoin.py
@@ -6,14 +6,14 @@ import os
 
 kcsbtc = "0"
 
-os.system("wget -O tick https://api.kucoin.com/v1/KCS-BTC/open/tick 2>/dev/null")
+os.system("wget -O tick https://api.kucoin.com/api/v1/market/stats?symbol=KCS-BTC 2>/dev/null")
 with open('tick', 'r') as myfile:
 
     url = myfile.read()
     #print(url)
     #data = json.loads(url.read().decode())
     data = json.loads(url)
-    kcsbtc = data['data']['lastDealPrice']
+    kcsbtc = data['data']['last']
 
     f = open("kcs.csv", "w")
     out = str(kcsbtc)
